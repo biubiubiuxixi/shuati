a4059cdd0a9ccafbd2937ffddfa6ded5228b86ab
biubiubiuxixi <843424294@qq.com>
2018-04-13 16:45:20 +0800

    '插入排序'

--- insertSort.js
+++ insertSort.js
@@ -1,33 +1,26 @@
-function insertSort(a, n)
-{
-    for (var i = 1; i < n; i++)
-    {
-        //为a[i]在前面的a[0...i-1]有序区间中找一个合适的位置
-        for (var j = i - 1; j >= 0; j--)
-            if (a[j] < a[i])
-                break;
+/**
+ * 直接插入排序
+ */
+var a = [1, 2, 8, 9, 2, 3, 6, 5, 4];
+var n = a.length;
+console.log('原来的数组' + a.map((i) => i));
+insertSort(a, n)
+console.log('排序后的数组' + a.map((i) => i));
 
-        //如找到了一个合适的位置
-        if (j != i - 1)
-        {
-            //将比a[i]大的数据向后移
-            var temp = a[i];
-            for (var k = i - 1; k > j; k--)
-                a[k + 1] = a[k];
-            //将a[i]放到正确位置上
-            a[k + 1] = temp;
+function insertSort(a, n) {
+    for (let i=1;i<n;i++) {
+        for (var j = i-1;j>=0;j--) {
+            if (a[j]<a[i]) {
+                break;
+            }
+        }
+        if (j != i-1) {
+            // 将比a[i]大的数字往后移
+            let temp = a[i];
+            for (var k = i-1;k>j;k--) {
+                a[k+1]=a[k];
+            }
+            a[k+1]=temp;
         }
     }
 }
-
-var a = [20,40,30,10,60,50];
-var ilen = a.length;
-console.log("原来的数组：");
-for (i=0; i<ilen; i++)
-    console.log(a[i]);
-
-insertSort(a, ilen);
-
-console.log("排序后的数组：")
-for (i=0; i<ilen; i++)
-    console.log(a[i]);
